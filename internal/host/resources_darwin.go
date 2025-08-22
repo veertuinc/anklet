a3d872782e7233d6b15a45ab333658a599ffc3a9
Nathan Pierce <norsegaud@gmail.com>
2025-08-22 11:26:46 -0500

    https://github.com/veertuinc/anklet/issues/8

--- internal/host/resources_darwin.go
+++ internal/host/resources_darwin.go
@@ -6,6 +6,7 @@ package host
 import "C"
 import (
 	"context"
+	"syscall"
 )
 
 // GetHostCPUCount returns the number of CPUs on the host
@@ -17,4 +18,36 @@ func GetHostCPUCount(pluginCtx context.Context) (int, error) {
 // GetHostMemoryBytes returns the total memory in bytes on the host
 func GetHostMemoryBytes(pluginCtx context.Context) (uint64, error) {
 	return uint64(C.sysconf(C._SC_PHYS_PAGES) * C.sysconf(C._SC_PAGE_SIZE)), nil
+}
+
+// GetHostDiskTotalBytes returns the total disk space in bytes
+func GetHostDiskTotalBytes(pluginCtx context.Context) (uint64, error) {
+	var stat syscall.Statfs_t
+	err := syscall.Statfs("/", &stat)
+	if err != nil {
+		return 0, err
+	}
+	return uint64(stat.Blocks) * uint64(stat.Bsize), nil
+}
+
+// GetHostDiskUsedBytes returns the used disk space in bytes
+func GetHostDiskUsedBytes(pluginCtx context.Context) (uint64, error) {
+	var stat syscall.Statfs_t
+	err := syscall.Statfs("/", &stat)
+	if err != nil {
+		return 0, err
+	}
+	total := uint64(stat.Blocks) * uint64(stat.Bsize)
+	available := uint64(stat.Bavail) * uint64(stat.Bsize)
+	return total - available, nil
+}
+
+// GetHostDiskAvailableBytes returns the available disk space in bytes
+func GetHostDiskAvailableBytes(pluginCtx context.Context) (uint64, error) {
+	var stat syscall.Statfs_t
+	err := syscall.Statfs("/", &stat)
+	if err != nil {
+		return 0, err
+	}
+	return uint64(stat.Bavail) * uint64(stat.Bsize), nil
 }
